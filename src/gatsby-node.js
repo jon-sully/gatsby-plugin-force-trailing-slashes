e7a9e2f47a6a3d48d66b6734faa2ef87f5dea3a6
Bay Phillips <bay.phillips@gmail.com>
2018-08-28 22:40:49 -0400

    allow some paths to be excluded

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,14 +1,21 @@
+const _ = require(`lodash`)
 const replacePath = _path => (_path === `/` ? _path : _path.replace(/\/$|$/, `/`))
 
-exports.onCreatePage = ({ page, actions }) => {
+exports.onCreatePage = ({ page, actions }, pluginOptions) => {
   const { createPage, deletePage } = actions
-
+  const options = {
+    excludedPaths: [`404.html`]
+  }
+  const combinedOptions = _.defaults(pluginOptions, options)
+  
   return new Promise(resolve => {
-    const oldPage = Object.assign({}, page)
-    page.path = replacePath(page.path)
-    if (page.path !== oldPage.path) {
-      deletePage(oldPage)
-      createPage(page)
+    if(!combinedOptions.excludedPaths.includes(page.path)) {
+      const oldPage = Object.assign({}, page)
+      page.path = replacePath(page.path)
+      if (page.path !== oldPage.path) {
+        deletePage(oldPage)
+        createPage(page)
+      }
     }
     resolve()
   })
